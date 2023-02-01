e20fca21377901488b8365fd84d3db0b478ff7c6
Gareth Ma <grhkm21@gmail.com>
2023-02-01 22:39:57 +0000

    Experimenting with prime_pi

--- sketch/prime_pi.lean
+++ sketch/prime_pi.lean
@@ -1,3 +1,4 @@
+import tactic
 import data.nat.prime_norm_num
 import number_theory.prime_counting
 
@@ -26,10 +27,39 @@ begin
   unfold list.countp,
 end
 
-example : π 400 = 25 :=
+lemma adjoin_range {n : ℕ} : ∀ l, list.range_core n l = (list.range_core n list.nil) ++ l :=
 begin
-  rw [prime_counting, prime_counting', count, list.range],
-  simp only [list.range_core, list.countp], -- recursively simp
+  induction n with k hk; intros a,
+  { refl, },
+  { dsimp [list.range_core],
+    rw [hk [k], hk (k :: a), list.append_assoc, list.append_right_inj, list.singleton_append], },
+end
+
+lemma countp_singleton {α : Type} {p : α → Prop} [decidable_eq α] [decidable_pred p] (a : α) :
+list.countp p [a] = ite (p a) 1 0 := rfl
+
+lemma countp_range {n : ℕ} {p : ℕ → Prop} [decidable_pred p] :
+list.countp p (list.range n.succ) = (ite (p n) 1 0) + list.countp p (list.range n) :=
+begin
+  dsimp [list.range],
+  simp only [list.range_core, adjoin_range [n], list.countp_append, add_comm, add_left_cancel_iff],
+  refl,
+end
+
+-- Benchmark: 14.2s, very slow
+set_option profiler true
+example {n : ℕ} : π 200 = 46 :=
+begin
+  rw [prime_counting, prime_counting', count],
+  repeat { rw countp_range, norm_num1, simp only [if_false, if_true, zero_add], },
+  rw [list.range_zero, list.countp_nil],
+end
+
+-- Benchmark: 949ms, quite fast
+example {n : ℕ} : π 200 = 46 :=
+begin
+  rw [prime_counting, prime_counting', count],
+  dsimp [list.range, list.range_core, list.countp],
   norm_num,
 end
 
